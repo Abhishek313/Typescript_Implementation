4a71e50d6ab9ca628b275470ed88ccd465c20296
Abhishek313 <vabhi5149@gmail.com>
2024-03-24 23:12:09 +0530

    initial files

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// let a :number= 25;
+const func = (a, b) => a * b;
+console.log(func(4, 3));
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+// let a :number= 25;
+
+// let nambo : string = "abhi"
+
+// let sirname: string | number = "hellow"
+
+
+// type UserName = string | number;
+
+// const func = (n : number,m : number):number => {
+//    return n*m;
+// }
+
+// let arr : number [] = [1,2,3,4,5]
+
+// let arr3 : Array<string|number> = [2,4,"ahdit"]
+
+// interface Obj {
+//     height: number;
+//     weight: number;
+//     gender?: boolean;
+// }
+
+// interface NewObj extends Obj {
+//     scholar : boolean;
+// }
+
+// const obj: Obj = {
+//     height: 44,
+//     weight: 433,
+// }
+
+
+
+
+type funcType = (m:number,n:number) => number;
+
+
+const func:funcType = (a,b) => a*b;
+
+
+console.log(func(4,3));
+
+
+
+
+
+
+
